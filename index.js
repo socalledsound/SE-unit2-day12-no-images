677ea7c4d0be5ed672ec3e3f5d9ca6e5d1fd8f1f
socalled kubick <socalledsound@gmail.com>
2020-10-02 08:04:41 -0700

    green ball starter

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+//some valus for the ground
+const groundOptions = {
+    y: 550,
+    fillColor: 'brown'
+}
+
+
+const greenBallOptions = {
+    x: 400,
+    y: 530,
+    r: 20,
+    fillColor: [0, 250, 0],
+
+}
+
+
 //some starting values for the playerball
 const playerBallOptions = {
     x: 100, 
@@ -8,6 +24,8 @@ const playerBallOptions = {
 //make a new player ball
 const myPlayerBall = new PlayerBall(playerBallOptions); 
 
+//initiliaze a green ball -- how can we make an array of three green balls?
+const greenBall = new Ball(greenBallOptions);
 
 
 function setup(){
@@ -16,7 +34,10 @@ function setup(){
 
 function draw(){
     background(210,220,240);
-   
+    //draw ground
+    drawGround(groundOptions.y, groundOptions.fillColor);
+    //show green ball -- how can we change this next line to draw an array of balls?
+    greenBall.display();
     //check the various arrow keys and move player if they are being pressed
     checkKeyInput();
     //display current position of player ball.
@@ -36,12 +57,17 @@ function checkKeyInput(){
         myPlayerBall.move('x', -1);
     }
 
-    if(keyIsDown(UP_ARROW)){
-        myPlayerBall.move('y', -1);
-    }
+    // if(keyIsDown(UP_ARROW)){
+    //     myPlayerBall.move('y', -1);
+    // }
 
-    if(keyIsDown(DOWN_ARROW)){
-        myPlayerBall.move('y', 1);
-    }
+    // if(keyIsDown(DOWN_ARROW)){
+    //     myPlayerBall.move('y', 1);
+    // }
 }
 
+//just a simple function to add the ground, it's not going to be changing much (at this point)
+drawGround = (y, fillColor) => {
+    fill(fillColor);
+    rect(0, y, width, height-y);
+}
