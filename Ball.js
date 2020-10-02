cb175e0b33baee1d811d9c918394822761dd5e5b
socalled kubick <socalledsound@gmail.com>
2020-10-01 22:13:49 -0700

    moving playerball with comments

--- Ball.js
+++ Ball.js
@@ -1,6 +1,6 @@
-
+//the main ball class
 class Ball {
-    constructor(x, y, r, fillColor){
+    constructor({x, y, r, fillColor}){
         this.x = x;
         this.y = y;
         this. r = r;
@@ -12,12 +12,15 @@ class Ball {
         ellipse(this.x, this.y, this.r * 2);
     }
 }
-
+//the playerball class derived from the main ball class
 class PlayerBall extends Ball {
-    constructor(x, y, r, fillColor) {
-        super(x, y, r, fillColor)
+    // take the arguments in to the constructor function
+    constructor({x, y, r, fillColor}) {
+        //assign them to values in the main Ball object
+        super({x, y, r, fillColor})
     }
 
+    //our new move function that only belongs to the player ball
     move(axis, amount){
         if(axis === 'x'){
             this.x += amount;
--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-//const myLittleGreenBall = new Ball(100,100,20, [0, 250, 0]);
-const myPlayerBall = new PlayerBall(300,300,100, [0, 0, 250]); 
+//some starting values for the playerball
+const playerBallOptions = {
+    x: 100, 
+    y: 500,
+    r: 50,
+    fillColor: [0, 0, 250],
+}
+//make a new player ball
+const myPlayerBall = new PlayerBall(playerBallOptions); 
+
+
 
 function setup(){
     createCanvas(600, 600);
@@ -7,23 +16,32 @@ function setup(){
 
 function draw(){
     background(210,220,240);
-    //myLittleGreenBall.display();
-    
-    
-    if(keyIsDown(RIGHT_ARROW)){
-        myPlayerBall.move('x', 1);
-    }
-
-
+   
+    //check the various arrow keys and move player if they are being pressed
+    checkKeyInput();
+    //display current position of player ball.
     myPlayerBall.display();
-
-
-
-
-
 
 
 
 }
 
+
+function checkKeyInput(){
+    if(keyIsDown(RIGHT_ARROW)){
+        myPlayerBall.move('x', 1);
+    }
+
+    if(keyIsDown(LEFT_ARROW)){
+        myPlayerBall.move('x', -1);
+    }
+
+    if(keyIsDown(UP_ARROW)){
+        myPlayerBall.move('y', -1);
+    }
+
+    if(keyIsDown(DOWN_ARROW)){
+        myPlayerBall.move('y', 1);
+    }
+}
 
